456098be06f8e73d3e5c9f904ec1c0e07e438ef9
GarryWhite <3284263390@qq.com>
2025-11-14 17:35:18 +0800

    feat(main): 添加LED闪烁功能并优化GPIO初始化
    重构GPIO初始化代码，将GPIO_Pin设置移到结构体初始化后
    添加LED循环闪烁功能，使用Mdelay_Lib实现1秒延时
    更新程序大小统计信息(Code=1108, RW-data=24)

--- src/main.c
+++ src/main.c
@@ -7,10 +7,11 @@ int main()
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
 	
 	GPIO_InitTypeDef g;
-	g.GPIO_Pin = GPIO_Pin_9;
 	g.GPIO_Mode = GPIO_Mode_OUT;
 	g.GPIO_Speed = GPIO_Speed_50MHz;
 	g.GPIO_OType = GPIO_OType_PP;
+	
+	g.GPIO_Pin = GPIO_Pin_9;
 	GPIO_Init(GPIOF, &g);
 	
 	g.GPIO_Pin = GPIO_Pin_10;
@@ -22,9 +23,26 @@ int main()
 	g.GPIO_Pin = GPIO_Pin_14;
 	GPIO_Init(GPIOE, &g);
 	
-	GPIO_ResetBits(GPIOF, GPIO_Pin_9);
-	GPIO_ResetBits(GPIOF, GPIO_Pin_10);
-	GPIO_ResetBits(GPIOE, GPIO_Pin_13);
-	GPIO_ResetBits(GPIOE, GPIO_Pin_14);
-	while(1);	
+	GPIO_SetBits(GPIOF, GPIO_Pin_9);
+	GPIO_SetBits(GPIOF, GPIO_Pin_10);
+	GPIO_SetBits(GPIOE, GPIO_Pin_13);
+	GPIO_SetBits(GPIOE, GPIO_Pin_14);
+	while(1)
+	{
+		GPIO_ResetBits(GPIOE, GPIO_Pin_14);
+		Mdelay_Lib(1000);
+		GPIO_SetBits(GPIOE, GPIO_Pin_14);
+		
+		GPIO_ResetBits(GPIOE, GPIO_Pin_13);
+		Mdelay_Lib(1000);
+		GPIO_SetBits(GPIOE, GPIO_Pin_13);
+
+		GPIO_ResetBits(GPIOF, GPIO_Pin_10);
+		Mdelay_Lib(1000);
+		GPIO_SetBits(GPIOF, GPIO_Pin_10);
+
+		GPIO_ResetBits(GPIOF, GPIO_Pin_9);
+		Mdelay_Lib(1000);
+		GPIO_SetBits(GPIOF, GPIO_Pin_9);
+	}
 }
